7b8e1fa8dda23ac3fee311f2db5063b55ca047e3
Yashvardhan Singh <79207707+yvs2701@users.noreply.github.com>
2021-07-01 14:45:03 +0530

    Add files via upload

---
+++ day 4/google-maps-services-py/noxfile.py
@@ -0,0 +1,67 @@
+import nox
+
+SUPPORTED_PY_VERSIONS = ["3.5", "3.6", "3.7", "3.8"]
+
+
+def _install_dev_packages(session):
+    session.install("-e", ".")
+
+
+def _install_test_dependencies(session):
+    session.install("pytest")
+    session.install("pytest-cov")
+    session.install("responses")
+
+
+def _install_doc_dependencies(session):
+    session.install("sphinx")
+
+
+@nox.session(python=SUPPORTED_PY_VERSIONS)
+def tests(session):
+    _install_dev_packages(session)
+    _install_test_dependencies(session)
+
+    session.install("pytest")
+    session.run("pytest")
+
+    session.notify("cover")
+
+
+@nox.session
+def cover(session):
+    """Coverage analysis."""
+    session.install("coverage")
+    session.install("codecov")
+    session.run("coverage", "report", "--show-missing")
+    session.run("codecov")
+    session.run("coverage", "erase")
+
+
+@nox.session(python="3.6")
+def docs(session):
+    _install_dev_packages(session)
+    _install_doc_dependencies(session)
+
+    session.run("rm", "-rf", "docs/_build", external=True)
+
+    sphinx_args = [
+        "-a",
+        "-E",
+        "-b",
+        "html",
+        "-d",
+        "docs/_build/doctrees",
+        "docs",
+        "docs/_build/html",
+    ]
+
+    sphinx_cmd = "sphinx-build"
+
+    session.run(sphinx_cmd, *sphinx_args)
+
+
+@nox.session()
+def distribution(session):
+    session.run("bash", ".github/scripts/distribution.sh", external=True)
+    session.run("python", "-c", "import googlemaps")
---
+++ day 4/google-maps-services-py/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+
+
+requirements = ["requests>=2.20.0,<3.0"]
+
+with open("README.md") as f:
+    readme = f.read()
+
+with open("CHANGELOG.md") as f:
+    changelog = f.read()
+
+
+setup(
+    name="googlemaps",
+    version="4.4.5",
+    description="Python client library for Google Maps Platform",
+    long_description=readme + changelog,
+    long_description_content_type="text/markdown",
+    scripts=[],
+    url="https://github.com/googlemaps/google-maps-services-python",
+    packages=["googlemaps"],
+    license="Apache 2.0",
+    platforms="Posix; MacOS X; Windows",
+    setup_requires=requirements,
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet",
+    ],
+    python_requires='>=3.5'
+)
